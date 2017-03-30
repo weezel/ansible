0155255a1ff266d700486b6ac73c36fffb9ec687
Ville Valkonen <weezelding@gmail.com>
2017-03-30 16:52:12 +0300

    Helper scripts for the deployment.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ANSIBLE=$(which ansible)
+ANSIBLE_PLAYBOOK=$(which ansible-playbook)
+
+usage()
+{
+	printf "usage: %s: inventoryfile tags playbookname\n" ${0}
+	exit 1
+}
+
+if [ $# -ne 3 ]; then
+	usage
+fi
+
+if [ ! -r "${1}" ]; then
+	echo "Error: File ${1} doesn't exist"
+	exit 1
+fi
+if [ ! -r "${3}" ]; then
+	echo "Error: File ${3} doesn't exist"
+	exit 1
+fi
+
+${ANSIBLE_PLAYBOOK} -i "${1}" -kK -t "${2}" "${3}"
+
---
+++ getfacts.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Parameters:
+# - local_facts
+# - hostname
+#
+# The first option local_facts gathers facts from the local computer.
+# Surprisingly, the second option will gather facts from the remote host(s).
+
+ANSIBLE=$(which ansible)
+
+usage()
+{
+	printf "usage: %s: [subset|local_facts] [inventoryfile]\n" ${0}
+	exit 1
+}
+
+if [ $# -eq 1 ]; then
+	if [ "${1}" = "local_facts" ]; then
+		${ANSIBLE} localhost --connection=local \
+			-m setup --tree localfacts
+	else
+		usage
+	fi
+elif [ $# -eq 2 ]; then
+	if [ ! -r "${2}" ]; then
+		echo "Error: File ${2} doesn't exist"
+		exit 1
+	fi
+	${ANSIBLE} all -l "${1}" -i "${2}" -k -m setup --tree remofacts
+else
+	usage
+fi
+
